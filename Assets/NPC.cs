924d72b862228cd1cb72090b679beb0c0ec46abb
msdd <56486103+3sdd@users.noreply.github.com>
2025-02-07 15:42:37 +0900

    c側でblackboardの変更検知試した

--- Assets/NPC.cs
+++ Assets/NPC.cs
@@ -6,20 +6,51 @@ public class NPC : MonoBehaviour
     [SerializeField]
     BehaviorGraphAgent _behaviorAgent;
 
+    BlackboardVariable<string> _behaviorAgentMyString;
+
     private void Awake()
     {
-        // _behaviorAgent.enabled = false;
-    }
-    void Start()
-    {
-        //_behaviorAgent.Start();
-        //_behaviorAgent.Restart();
-        // _behaviorAgent.enabled = true;
+        _behaviorAgent.enabled = false;
     }
 
-    void Update(){
-        if(Input.GetKeyDown(KeyCode.Space)){
-            _behaviorAgent.End();
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartNpc();
+
+            Debug.Log("MyString at start: "+_behaviorAgentMyString.Value);
         }
+    }
+
+    void OnEnable()
+    {
+        //   if (m_Agent.BlackboardReference.GetVariable("StateEventChannel", out m_stateEventChannelBBV))
+        //         m_stateEventChannelBBV.Value.Event += OnStateEvent;
+
+        if (_behaviorAgent.BlackboardReference.GetVariable("MyString", out _behaviorAgentMyString))
+        {
+            _behaviorAgentMyString.OnValueChanged += OnMyStringValueChanged;
+        }
+    }
+
+    void OnDisable()
+    {
+        if (_behaviorAgentMyString != null)
+        {
+            _behaviorAgentMyString.OnValueChanged -= OnMyStringValueChanged;
+        }
+    }
+
+    void StartNpc()
+    {
+        _behaviorAgent.enabled = true;
+    }
+
+    void OnMyStringValueChanged()
+    {
+        var newValue=_behaviorAgentMyString.Value;
+        Debug.Log("OnMyStringValueChanged called: "+newValue);
     }
 }
